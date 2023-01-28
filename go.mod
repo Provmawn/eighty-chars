28ffcbeb73dcdc32bb252f7c93884ccea72d2eea
provmawn <provmawn@gmail.com>
2023-01-28 05:25:58 -0800

    add main.go and go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Provmawn/eighty-chars
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func ReadFileContents(path string) string {
+    data, err := os.ReadFile(path)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return string(data)
+}
+
+func Format(contents string, characterLimit int) string {
+    contents = strings.Trim(contents, " ")
+    lines := strings.Split(contents, "\n")
+    for i, line := range lines {
+        if len(line) > characterLimit {
+            for j := characterLimit; j > 0; j-- {
+                if unicode.IsSpace(rune(line[j])) {
+                    lines[i] = line[:j] + "\n" + line[j+1:] 
+                    break
+                }
+            }
+        }
+    }
+   return strings.Join(lines, "\n")
+}
+
+func main() {
+    file := flag.String("file", "", "file that we will run formatting on")
+    output := flag.String("output", *file, "name of file to output formatted content")
+    charLimit := flag.Int("char-limit", 80, "lengths limit of each line")
+    flag.Parse()
+    contents := ReadFileContents(*file)
+    contents = Format(contents, *charLimit)
+    if *output != "" {
+        os.WriteFile(*output, []byte(contents), 0664)
+    }
+}
